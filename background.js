ce9ae45e5f46ef9099ed5a7a2dfa9ebbc5ee6f71
philippkueng <git@philippkueng.ch>
2012-09-08 21:40:05 +0200

    added badgetext to notifiy the user about the action currently being executed

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab){
+	chrome.browserAction.setBadgeText({
+		text: "..."
+	});
+
 	chrome.tabs.captureVisibleTab(null, {format: "png"}, function(dataUrl){
 
 		var xhr = new XMLHttpRequest();
@@ -7,8 +11,8 @@ chrome.browserAction.onClicked.addListener(function(tab){
 		formData.append("password", localStorage["password"]);
 		formData.append("img", dataUrl);
 
-		// xhr.open("POST", "https://bitfondue-api.herokuapp.com/upload", true);
-		xhr.open("POST", "http://localhost:5000/upload", true);
+		xhr.open("POST", "https://bitfondue-api.herokuapp.com/upload", true);
+		// xhr.open("POST", "http://localhost:5000/upload", true);
 
 		chrome.tabs.getSelected(null, function(tab){
 			formData.append("tab_info", JSON.stringify(tab));
@@ -21,6 +25,19 @@ chrome.browserAction.onClicked.addListener(function(tab){
 				console.log(response);
 				if(response.method == "getHTML"){
 					formData.append("html", response.data);
+					xhr.onreadystatechange = function(){
+						if(xhr.readyState == 4){
+							chrome.browserAction.setBadgeText({
+								text: "ok"
+							});
+
+							setTimeout(function(){
+								chrome.browserAction.setBadgeText({
+									text: ""
+								})
+							}, 2000);
+						}
+					};
 					xhr.send(formData);
 				}
 			});
